9ef81d9188e83f8d20d030d5ce3eb6cbe44da658
Pranjul Gupta <pranjul.here@gmail.com>
2025-11-21 18:37:42 +0530

    wrote the main logic.

---
+++ HotelRoomBookingSystem.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+struct Room{
+    int RoomNum;
+    char guestName[100];
+    char roomType[100];
+    double RentPerNight;
+    int status;
+};
+
+int roomCount = 0;
+Room Records[100];
+
+void checkIN();
+void checkOUT();
+void AvailableRooms();
+void SearchGuest();
+void saveDataToFile();
+void LoadDataFromFile();
+
+int main(){
+
+    LoadDataFromFile();
+
+    int choice;
+
+    do{
+        cout << "-----Hotel Room Booking-------" << endl;
+        cout << "1.Check In" << endl;
+        cout << "2.Check Out" << endl;
+        cout << "3.Show Available Rooms" << endl;
+        cout << "4.Search Guest" << endl;
+        cout << "0.Exit" << endl;
+        cout << "------------------------------" << endl;
+        cout << "Enter you choice : ";
+        cin >> choice;
+
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "Invalid Choice. Kindly Retry." << endl;
+            continue;
+        }
+
+        switch (choice) {
+            case 1:
+                checkIN();
+                break;
+            case 2:
+                checkOUT();
+                break;
+            case 3:
+                AvailableRooms();
+                break;
+            case 4:
+                SearchGuest();
+                break;
+            case 0:
+                saveDataToFile();
+                cout << "Saving and Exiting." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please try again." << endl;
+                break;
+                
+        }
+    }
+    while(choice != 0);
+}
